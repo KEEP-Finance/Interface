10fcf9f970533640b712a2caf4af0571372fec49
doctor <wangzhe18@jd.com>
2022-04-22 09:02:49 +0800

    add basefile

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,23 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/BasicLayout',
+      routes: [
+        {
+          path: '/',
+          title: 'Keey',
+          component: '@/pages/Home/index',
+        },
+        {
+          path: '/home2',
+          title: 'Keey',
+          component: '@/pages/Home2/index',
+        },
+      ],
+    },
+  ],
   fastRefresh: {},
 });
